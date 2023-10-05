21d08096f2150bdbc5364dd50debf059fe0976fc
Zhaohao Lu <1184185682@qq.com>
2023-10-05 21:22:58 +1100

    使用NextResponse代替了Response

--- app/api/chat-stream/route.ts
+++ app/api/chat-stream/route.ts
@@ -1,6 +1,5 @@
 import { createParser } from "eventsource-parser";
-import { NextRequest } from "next/server";
-
+import {NextRequest, NextResponse} from "next/server";
 
 // 创建一个异步函数来生成一个可读流
 async function createStream(payload: ReadableStream<Uint8Array>) {
@@ -10,10 +9,11 @@ async function createStream(payload: ReadableStream<Uint8Array>) {
 
   //在控制台打印
     console.log("Before sending request");
-  
+
   // 发起带有有效负载的 POST 请求到 LLM 服务
-  const res = await fetch("http://region-8.autodl.pro:47847/v1/chat/completions", { //芜湖区 / 003 机
+  // const res = await fetch("http://region-8.autodl.pro:47847/v1/chat/completions", { //芜湖区 / 003 机
     // const res = await fetch("http://region-31.seetacloud.com:46766/v1/chat/completions", { //内蒙 A 区 / 385 机
+    const res = await fetch("http://region-31.seetacloud.com:39349/v1/chat/completions", { //内蒙A区 / 367机 A40显卡
 
     headers: {
       "Content-Type": "application/json",
@@ -63,16 +63,15 @@ async function createStream(payload: ReadableStream<Uint8Array>) {
 // 处理 POST 请求的处理程序
 export async function POST(req: NextRequest) {
   try {
-    // 创建流并将其返回作为响应
     const stream = await createStream(req.body!);
-    return new Response(stream);
+    return new NextResponse(stream, { headers: { 'Content-Type': 'text/html; charset=utf-8' } }); // 修改这里，使用NextResponse代替Response.添加响应头
+
   } catch (error) {
-    // 如果出现错误，记录错误信息
     console.error("[Chat Stream]", error);
+    return new NextResponse('Error', { status: 500 }); // 如果出现错误，返回一个带有错误状态的响应
   }
 }
 
-// 定义 Next.js 配置
 export const config = {
-  // runtime: "edge",
+  runtime: "edge",
 };
